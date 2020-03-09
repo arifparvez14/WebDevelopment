e14ae3ed75708d8ef3874c82315bc551b6b25659
Arif <arif.mbstu22@gmail.com>
2020-03-09 12:52:33 +0600

    double vs triple equal added.

---
+++ Advanced JavaScript/double-vs-triple-equal.js
@@ -0,0 +1,18 @@
+//"==" check value.
+//"===" check value & type.
+
+const first = 0;
+const second = false;
+if (first == second){
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
+
+const first2 = 3;
+const second2 = "3";
+if (first2 == second2){
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
