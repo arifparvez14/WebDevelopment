bc9c173f5215734b5a44bc509f3d1b6a21ff0e37
Arif <arif.mbstu22@gmail.com>
2020-03-09 12:35:38 +0600

    Null vs undefined clear.

---
+++ Advanced JavaScript/nullVSundefined.js
@@ -0,0 +1,33 @@
+//Undefined cases
+
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+const res = add(10,15);
+console.log(res);
+
+
+function add2(num1, num2){
+    console.log(num1, num2);
+}
+const res2 = add2(10);
+console.log(res2);
+
+let premik = {name: "Abdul", phoneNo: 0154212};
+console.log(premik.gf);
+
+let val = undefined;
+console.log(val);
+
+let age = [2,4,5];
+console.log(age[11]);
+
+//Null
+let vari = null;
+console.log(vari);
+
