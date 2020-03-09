45a5c5f859a82d25ca67307146ae5ced32fe6b68
Arif <arif.mbstu22@gmail.com>
2020-03-09 16:40:52 +0600

    Pioneer bank project added.

---
+++ Advanced JavaScript/map-filter.js
@@ -0,0 +1,11 @@
+const num = [3,4,5,7,8];
+// const output = [];
+
+// for (let i=0; i<num.length; i++){
+//     const element = num[i];
+//     const square = element * element;
+//     output.push(square);
+// }
+
+
+console.log(output);
