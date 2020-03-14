be223027cdbe9754df1cf636b858b15cd5aae0a1
TheGenial <arifulislam14.ict@gmail.com>
2020-03-15 02:37:16 +0600

    product details added
    product details added

--- Projects/ema-john-simple/src/components/NotFound/NotFound.js
+++ Projects/ema-john-simple/src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="not-found">
             <h1>Sorry, page not found</h1>
             <h3>404 Error!!!</h3>
         </div>
