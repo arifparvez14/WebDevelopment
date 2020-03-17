23b9cd8913d94d674e1b572d05feb3afab2f8d90
TheGenial <arifulislam14.ict@gmail.com>
2020-03-17 15:06:58 +0600

    firebase auth demo project added & config file added
    firebase auth demo project added & config file added

---
+++ Projects/firebase-auth/src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwRNuOKcEGa1c_5sLjfQrii6KkPbBmR2I",
+    authDomain: "ema-john-reactapp.firebaseapp.com",
+    databaseURL: "https://ema-john-reactapp.firebaseio.com",
+    projectId: "ema-john-reactapp",
+    storageBucket: "ema-john-reactapp.appspot.com",
+    messagingSenderId: "760566823402",
+    appId: "1:760566823402:web:4b6ffb1b04635b5a869a93"
+  };
+
+  export deafult firebaseConfig;
