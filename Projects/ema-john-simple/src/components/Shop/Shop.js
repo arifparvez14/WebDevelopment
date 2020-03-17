b3bd38558426be86902e1d5d8b8d3d50bdd74f55
TheGenial <arifulislam14.ict@gmail.com>
2020-03-17 11:24:58 +0600

    Order state fix in all page
    Order state fix in all page

--- Projects/ema-john-simple/src/components/Shop/Shop.js
+++ Projects/ema-john-simple/src/components/Shop/Shop.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDatabaseCart} from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart} from '../../utilities/databaseManager';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
+
+    useEffect(()=> {
+        const saveCart = getDatabaseCart();
+        const productKeys = Object.keys(saveCart);
+        const previousCart = productKeys.map(existingKey => {
+            const product = fakeData.find(pd => pd.key === existingKey);
+            product.quantity = saveCart[existingKey];
+            return product;
+        }) 
+        setCart(previousCart);
+    }, [])
 
     const handleAddProduct = (product) => {
         const toBeAddededKey = product.key;
