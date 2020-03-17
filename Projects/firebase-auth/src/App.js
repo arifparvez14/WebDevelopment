32e1417b43050db86e50157e9735e0cc1c17963a
TheGenial <arifulislam14.ict@gmail.com>
2020-03-17 20:23:44 +0600

    Regular exp added, form validatio work done
    Regular exp added, form validatio work done

--- Projects/firebase-auth/src/App.js
+++ Projects/firebase-auth/src/App.js
@@ -36,18 +36,51 @@ function App() {
       })
   }
   const handleSignOut = () => {
-    firebase.auth().signOut().
-    then(res => {
-      const signedOutUser = {
-        isSignedIn: false,
-        name: '',
-        email: '',
-        photo: ''
-      }
-      setUser(signedOutUser);
-    }).catch(err => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: '',
+          password: '',
+          isValid: false
+        }
+        setUser(signedOutUser);
+      }).catch(err => {
 
-    });
+      });
+  }
+
+  const is_valid_email = email =>  /(.+)@(.+){2,}\.(.+){2,}/.test(email);
+  const hasNumber = input => /\d/.test(input);
+
+  const handleChange = event => {
+    const newUserInfo = {
+      ...user
+    }
+
+    let isValid = true;
+    if (event.target.name === 'email'){
+      isValid = is_valid_email(event.target.value);
+    }
+
+    let isValidPassword = true;
+    if (event.target.name === 'password'){
+      isValid = event.target.value > 8 && hasNumber(event.target.value);
+    }
+
+    newUserInfo[event.target.name] = event.target.value;
+    newUserInfo.isValid = isValid
+    setUser(newUserInfo);
+  }
+
+  const createAccount = () => {
+    if(user.isValid){
+      console.log(user.email, user.password);
+    } else {
+      console.log('Form is not valid');
+    }
   }
 
   return (
@@ -63,6 +96,15 @@ function App() {
           <img src={user.photo} alt="" />
         </div>
       }
+      <h1>Our own authentication</h1>
+
+      <form onSubmit={createAccount}>
+        <input type="text" onBlur={handleChange} name="email" placeholder="Your Email" required />
+        <br />
+        <input type="password" onBlur={handleChange} name="password" placeholder="Your Password" required />
+        <br />
+        <input type="submit" value="Create Account" />
+      </form>
     </div>
   );
 }
