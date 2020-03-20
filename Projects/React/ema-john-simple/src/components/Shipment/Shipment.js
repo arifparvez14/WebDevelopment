ad721bce4b477cbeb089587e7b83bca52b73fa34
TheGenial <arifulislam14.ict@gmail.com>
2020-03-21 02:32:39 +0600

    react hook form added
    react hook form added

--- Projects/React/ema-john-simple/src/components/Shipment/Shipment.js
+++ Projects/React/ema-john-simple/src/components/Shipment/Shipment.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => { console.log(data) }
+
+    console.log(watch('example')) // watch input value by passing the name of it
+
     return (
-        <div>
-            <h1>This is shipment page.</h1>
-        </div>
-    );
+        < form onSubmit={handleSubmit(onSubmit)} >
+            < input name="example" defaultValue="test" ref={register} />
+            < input name="exampleRequired" ref={register({ required: true })} />
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input type="submit" />
+        </form >
+    )
 };
 
 export default Shipment;
