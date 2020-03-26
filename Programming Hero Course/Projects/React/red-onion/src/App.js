dba493ef435e73a0d9984b2eb8bb7bdd0c246e8e
TheGenial <arifulislam14.ict@gmail.com>
2020-03-26 20:52:15 +0600

    Header and Banner added
    Header and Banner added

--- Programming Hero Course/Projects/React/red-onion/src/App.js
+++ Programming Hero Course/Projects/React/red-onion/src/App.js
@@ -1,36 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider, PrivateRoute } from './Components/SignUp/useAuth';
 import './App.css';
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from './firebase.config';
-import Items from './Components/Items/Items';
-import Login from './Components/Login/Login';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Signup from './Components/Signup/Signup';
-firebase.initializeApp(firebaseConfig);
+import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
+
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
 
-          <Route path="/">
-            <Items></Items>
-          </Route>
-
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-
-          <Route path="/signup">
-            <Signup></Signup>
-          </Route>
-          
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="main">
+                    <Switch>
+                        <Route exact path="/">
+                            <Header></Header>
+                            <Banner></Banner>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
