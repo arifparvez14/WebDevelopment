5f1c950d9c56633adc79388767b91eb06efbe00b
TheGenial <arifulislam14.ict@gmail.com>
2020-03-29 17:01:53 +0600

    FoodItems shows in red onio app
    FoodItems shows in red onio app

--- Programming Hero Course/Projects/React/red-onion/src/App.js
+++ Programming Hero Course/Projects/React/red-onion/src/App.js
@@ -5,9 +5,44 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
 import SignUp from './Components/SignUp/SignUp';
+import Foods from './Components/Foods/Foods';
 
 
 function App() {
+
+    const [cart, setCart] = useState([]);
+    const [deliveryDetails, setDeliveryDetails] = useState({
+        todoor: null, road: null, flat: null, businessname: null, address: null
+    });
+
+    const deliveryDetailsHandler = (data) => { setDeliveryDetails(data);}
+    const clearCart = () => {setCart([]);}
+
+    const cartHandler = (data) => {
+        const alreadyAdded = cart.find(crt => crt.id == data.id);
+        const newCart = [...cart, data];
+        setCart(newCart);
+
+        if (alreadyAdded){
+            const remainingCarts = cart.filter(crt => cart.id != data);
+            setCart(remainingCarts);
+        } else {
+            const newCart = [...cart, data];
+            setCart(newCart);
+        }
+    }
+
+    const checkOutItemHandler = (productId, productQuantity) => {
+        const newCart = cart.map(item => {
+            if (item.id == productId){
+                item.quantity = productQuantity;
+            }
+            return item;
+        })
+
+        const filterdCart = newCart.filter(item => item.quantity > 0)
+        setCart(filterdCart);
+    }
 
     return (
         <AuthProvider>
@@ -17,6 +52,7 @@ function App() {
                         <Route exact path="/">
                             <Header></Header>
                             <Banner></Banner>
+                            <Foods cart={cart}></Foods>
                         </Route>
                         <Route path="/login">
                             <SignUp></SignUp>
